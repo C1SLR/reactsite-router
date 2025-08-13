75faedb97ec74f5f80f3b81d00dc608793f76424
C1SLR <umarfarooq7282003@gmail.com>
2025-08-13 16:13:50 +0500

    bugs and errors

--- src/components/search box/Search.jsx
+++ src/components/search box/Search.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Search() {
   return (
-    <div className="border flex justify-center">
+    <div className="flex justify-center">
       <input
         type="search"
         placeholder="Search Here..."
